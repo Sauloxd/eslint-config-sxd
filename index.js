c74782658209c7a80312b45a25599317b8a4fbe3
sauloxd <saulotoshi@gmail.com>
2019-12-27 12:00:52 -0300

    add typescript options

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
 };
